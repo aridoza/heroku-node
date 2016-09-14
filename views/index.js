e458fdb7a18916340f8f03f8da7cdf941cd42774
aridoza <aridoza85@gmail.com>
2016-09-13 20:57:55 -0400

    disable express static in server file

--- views/index.js
+++ views/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 
+console.log("Hello from React");
 
 ReactDOM.render(
   <h1>Hello from React!</h1>,
