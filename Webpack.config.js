a7f3294d8ebafaad2c60093c6d21f38762a83aa4
aridoza <aridoza85@gmail.com>
2016-09-13 21:42:03 -0400

    react setup

--- Webpack.config.js
+++ Webpack.config.js
@@ -1,17 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: path.join(__dirname, '/views/index.ejs'),
+  filename: 'index.ejs',
+  inject: 'body'
+})
+
 module.exports = {
   entry: [
     './index.js'
   ],
   output: {
-    path: __dirname,
+    path: path.join(__dirname, 'dist'),
     publicPath: '/',
     filename: 'bundle.js'
   },
+  plugins: [
+    HtmlWebpackPluginConfig
+  ],
   module: {
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
+      include: path.join(__dirname, 'server')
       query: {
         presets: ['es2015', 'react']
       }
