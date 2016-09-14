a02bd37f3c93c81978573748f785f98c38b669d0
aridoza <aridoza85@gmail.com>
2016-09-13 21:18:21 -0400

    tweaking react

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
+import App from '../app/containers/App';
 
 console.log("Hello from React");
 
 ReactDOM.render(
-  <h1>Hello from React!</h1>,
+  <App />,
   document.getElementById('scotchy-app')
 );
